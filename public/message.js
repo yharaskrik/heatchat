2af51631cc7722120ee7313a9dc859d376d8863b
yharaskrik <jaycooperbell@gmail.com>
2017-10-25 20:49:48 -0700

    messaging sending and displaying is now working. Will add messages to the correct side based on who sent them.
    Need to get autoscrolling working and fix UI stuff.

--- public/message.js
+++ public/message.js
@@ -1,4 +1,28 @@
-(function () {
+window.onload = function() {
+// Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCEEGAf_8ONezDvKqI5gLa4r57XLoH-qNE",
+        authDomain: "heatchat-184021.firebaseapp.com",
+        databaseURL: "https://heatchat-184021.firebaseio.com",
+        projectId: "heatchat-184021",
+        storageBucket: "heatchat-184021.appspot.com",
+        messagingSenderId: "1048720965159"
+    };
+    firebase.initializeApp(config);
+
+    var fbuser;
+    var messageIDs = [];
+
+    var textInput = document.querySelector('#message_input');
+    var postButton = document.querySelector('#send_message');
+
+    postButton.addEventListener("click", function () {
+        var msgText = textInput.value;
+        console.log(msgText);
+        firebase.database().ref('messages/').push({uid: fbuser.uid, text: msgText, time: Date.now()});
+        textInput.value = "";
+    });
+
     var Message;
     Message = function (arg) {
         this.text = arg.text, this.message_side = arg.message_side;
@@ -15,43 +39,53 @@
         }(this);
         return this;
     };
-    $(function () {
-        var getMessageText, message_side, sendMessage;
-        message_side = 'right';
-        getMessageText = function () {
-            var $message_input;
-            $message_input = $('.message_input');
-            return $message_input.val();
-        };
-        sendMessage = function (text) {
-            var $messages, message;
-            if (text.trim() === '') {
-                return;
-            }
-            $('.message_input').val('');
-            $messages = $('.messages');
-            message_side = message_side === 'left' ? 'right' : 'left';
-            message = new Message({
-                text: text,
-                message_side: message_side
+
+    var db_ref = firebase.database().ref('messages/').orderByChild('time').startAt(Date.now());
+    db_ref.on('value', function (snapshot) {
+        if (snapshot.val()) {
+            snapshot.forEach(function (childSnapshot) {
+
+                var currentMsg = childSnapshot.val();
+                if (messageIDs.indexOf(childSnapshot.key) === -1) {
+                    messageIDs.push(childSnapshot.key);
+
+                    if (currentMsg.uid === fbuser.uid) {
+                        currentMsg.side = 'right';
+                    }
+                    else {
+                        currentMsg.side = 'left';
+                    }
+                    var message = new Message({
+                        text: currentMsg.text,
+                        message_side: currentMsg.side
+                    });
+                    message.draw();
+                }
+
+
             });
-            message.draw();
-            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
-        };
-        $('.send_message').click(function (e) {
-            return sendMessage(getMessageText());
-        });
-        $('.message_input').keyup(function (e) {
-            if (e.which === 13) {
-                return sendMessage(getMessageText());
-            }
-        });
-        sendMessage('Hello Philip! :)');
-        setTimeout(function () {
-            return sendMessage('Hi Sandy! How are you?');
-        }, 1000);
-        return setTimeout(function () {
-            return sendMessage('I\'m fine, thank you!');
-        }, 2000);
+
+        }
+        var chatWindow = document.getElementById('chat_window');
+        chatWindow.scrollTop = chatWindow.scrollHeight;
     });
-}.call(this));
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            fbuser = user;
+            console.log(user);
+        }
+        else {
+            fbuser = null;
+        }
+    });
+
+    firebase.auth().signInAnonymously().catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+
+        // ...
+    });
+}
